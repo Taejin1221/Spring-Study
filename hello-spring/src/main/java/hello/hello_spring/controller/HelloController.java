47551e723a125f54b4293e92db7f551b53d6528c
taejin1221 <taejin7824@gmail.com>
2024-07-26 16:53:54 +0900

    스프링 입문 강의(Section 2: 스프링 웹 개발 기초) Code
    1. 정적 컨텐츠 반환 방식
    2. MVC와 템플릿 엔진 방식
    3. API 방식

--- hello-spring/src/main/java/hello/hello_spring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hello_spring/controller/HelloController.java
@@ -3,6 +3,8 @@ package hello.hello_spring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -11,5 +13,53 @@ public class HelloController {
         model.addAttribute("data", "hello!");
 
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMVC(@RequestParam("name") String name, @RequestParam("age") int age, Model model) {
+        model.addAttribute("name", name);
+        model.addAttribute("age", age);
+
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloAPI(@RequestParam("name") String name, @RequestParam("age") int age) {
+        Hello helloObj = new Hello(name, age);
+
+        return helloObj;
+    }
+
+    static class Hello {
+        private String name;
+        private int age;
+
+        Hello(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
     }
 }
